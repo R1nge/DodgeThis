99f028420c5ecbc35406a61eb8f2443189bc19e7
R1ngeDev <r1ngegamedev@gmail.com>
2023-01-01 17:14:06 +0300

    Added ammo

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,15 @@
+﻿using Character;
+using Unity.Netcode;
+using UnityEngine;
+
+public class Ammo : NetworkBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out CharacterWeapon weapon))
+        {
+            weapon.AddAmmoServerRpc();
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Character/CharacterWeapon.cs
+++ Assets/Scripts/Character/CharacterWeapon.cs
@@ -1,23 +1,35 @@
-﻿using UnityEngine;
+﻿using Unity.Netcode;
+using UnityEngine;
 
 namespace Character
 {
-    public class CharacterWeapon : MonoBehaviour
+    public class CharacterWeapon : NetworkBehaviour
     {
         [SerializeField] private Camera playerCamera;
         [SerializeField] private float distance;
+        private NetworkVariable<bool> _canShoot;
+
+        private void Awake() => _canShoot = new NetworkVariable<bool>();
 
         private void Update()
         {
             if (!Input.GetMouseButtonDown(0)) return;
+            if (!_canShoot.Value) return;
             Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
             if (Physics.Raycast(ray, out var hit, distance))
             {
                 if (hit.transform.TryGetComponent(out CharacterStun stun))
                 {
                     stun.StunServerRpc();
+                    ShootServerRpc();
                 }
             }
         }
+
+        [ServerRpc]
+        private void ShootServerRpc() => _canShoot.Value = false;
+
+        [ServerRpc(RequireOwnership = false)]
+        public void AddAmmoServerRpc() => _canShoot.Value = true;
     }
 }
