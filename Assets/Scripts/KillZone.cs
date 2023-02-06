05e5a9dd8a4085e4e72254362e348257d6eb7173
R1ngeDev <r1ngegamedev@gmail.com>
2023-02-06 20:17:30 +0300

    fix: kill zone client collision

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -6,11 +6,31 @@ public class KillZone : NetworkBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(!IsServer) return;
-        if (other.transform.TryGetComponent(out CharacterState character))
+        if (IsServer)
         {
-            if (!character.GetComponent<NetworkObject>().IsSpawned || character == null) return;
-            character.Kill(character.OwnerClientId);
+            if (other.transform.TryGetComponent(out CharacterState character))
+            {
+                if (!character.GetComponent<NetworkObject>().IsSpawned || character == null) return;
+                character.Kill(character.OwnerClientId);
+            }
+        }
+        else
+        {
+            OnCollisionServerRpc(other.GetComponent<NetworkObject>());
+        }
+        
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void OnCollisionServerRpc(NetworkObjectReference other)
+    {
+        if (other.TryGet(out NetworkObject obj))
+        {
+            if (obj.transform.TryGetComponent(out CharacterState character))
+            {
+                if (!character.GetComponent<NetworkObject>().IsSpawned || character == null) return;
+                character.Kill(character.OwnerClientId);
+            }
         }
     }
 }
